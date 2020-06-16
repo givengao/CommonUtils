c6dbf5e18daf89df98b31891aae419635dc0e7cb
zonghuigao <1511180210@qq.com>
2020-06-16 22:42:29 +0800

    1.tijiao1

--- db/src/main/java/com/zxyun/common/db/mysql/action/Delete.java
+++ db/src/main/java/com/zxyun/common/db/mysql/action/Delete.java
@@ -1,9 +1,11 @@
 package com.zxyun.common.db.mysql.action;
 
 /**
- * @des:
+ * @des: 删除相关操作
  * @Author: given
  * @Date 2020/4/10 10:59
  */
 public class Delete {
+    private String name;
+    private Integer
 }
